f04004c483d993c4e9f6c0edf72559fa269164c3
Melinda <mrs.melindamalone@gmail.com>
2021-09-25 14:58:33 -0500

    feat: add images and revised README

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,6 +1,20 @@
-library()
 library(dplyr)
-library(tidyverse)
-MechaCar_mpg <- MechaCar_mpg
+
+MechaCar_mpg <- read.csv("MechaCar_mpg.csv")
+
 ?lm
-lm(MechaCar_mpg)  
+Mechalm <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_mpg)  
+summary(Mechalm)
+
+Suspension_Coil <- read.csv("Suspension_Coil.csv")
+Suspension_Total <- Suspension_Coil %>% summarize(mean=mean(PSI), median=median(PSI), variance=var(PSI), std=sd(PSI))
+Suspension_Lot <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(mean=mean(PSI), median=median(PSI), variance=var(PSI), std=sd(PSI))
+
+write.csv(Suspension_Coil, "sc.csv", row.names = FALSE)
+write.csv(Suspension_Total, "st.csv", row.names = FALSE)
+write.csv(Suspension_Lot, "sl.csv", row.names = FALSE)
+
+t.test(Suspension_Coil$PSI, mu=1500)
+t.test(subset(Suspension_Coil, Manufacturing_Lot=="Lot1")$PSI, mu=1500)
+t.test(subset(Suspension_Coil, Manufacturing_Lot=="Lot2")$PSI, mu=1500)
+t.test(subset(Suspension_Coil, Manufacturing_Lot=="Lot3")$PSI, mu=1500)
--- README.md
+++ README.md
@@ -5,6 +5,6 @@ In Module Fifteen, R programming language, a language popular in data science an
 
 ## Linear Regression to Predict MPG
 
-The coefficients produced after using the lm() function on the MechaCar_mpg dataframe are ground_clearance, vehicle_weight, AWD, spoiler_angle, and mpg.
+Based on the Linear Regression Model for the MechaCar Statistical Analysis, it has been determined that vehicle_length and ground_clearance impact the mpg the most of all the coefficients.  The slope of the MechaCar Linear Regression Model is not zero because the $R^2$ is 71%
 
-![](Images/MechaCar_mpg.PNG)
+![](Images/MechaCar_Linear_Model.PNG)
